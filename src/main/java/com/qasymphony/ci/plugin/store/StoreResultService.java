b019b38e6b04e393faaa0d7234d71cdc6932c01d
Trong Le <trongle@kms-technology.com>
2015-12-07 10:23:52 +0700

    add request and result in fetch method

--- src/main/java/com/qasymphony/ci/plugin/store/StoreResultService.java
+++ src/main/java/com/qasymphony/ci/plugin/store/StoreResultService.java
@@ -4,8 +4,6 @@ import com.qasymphony.ci.plugin.exception.StoreResultException;
 import com.qasymphony.ci.plugin.model.SubmittedResult;
 import hudson.model.AbstractProject;
 
-import java.util.Map;
-
 /**
  * @author trongle
  * @version 10/22/2015 9:20 PM trongle $
@@ -25,10 +23,20 @@ public interface StoreResultService {
   /**
    * Load all result
    *
-   * @param project
+   * @param request
    * @return
    * @throws StoreResultException
    */
-  Map<Integer, SubmittedResult> fetchAll(AbstractProject project, int currentBuildNumber)
+  ReadSubmitLogResult fetchAll(ReadSubmitLogRequest request)
+    throws StoreResultException;
+
+  /**
+   * Fetch with paging and search from file.
+   *
+   * @param request
+   * @return
+   * @throws StoreResultException
+   */
+  ReadSubmitLogResult fetch(ReadSubmitLogRequest request)
     throws StoreResultException;
 }
