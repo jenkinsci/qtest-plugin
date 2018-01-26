4698b6c6a5fa966810c94500979fa4fcb5abee68
Phu Ta <phuta@qasymphony.com>
2018-01-26 11:02:07 +0700

    fix layout

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -316,7 +316,7 @@ function buildTree(jItem, data, qTestParentId) {
 //            divMainItem.append(aLink);
 
             var divContent = $j("<div class='content'></div>");
-            //divContent.attr("title", (element.pid || "").toUpperCase() + " " + element.name)
+            divContent.attr("title", (element.pid || "").toUpperCase() + " " + element.name)
             divContent.text((element.pid || "").toUpperCase() + " " + element.name);
             divContent.attr("qtestid", element.id);
             divContent.attr("qtesttype", element.type);
